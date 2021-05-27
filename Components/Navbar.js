9a830753c6b6d6ecbb75d09279511194fa34f11e
Oscark021 <oscarvelasco169@gmail.com>
2021-05-27 18:46:25 -0500

    :sparkles: Se creo barra de navegación

---
+++ Components/Navbar.js
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+
+const Navbar = () => {
+    return(
+        <nav>
+            <Link href="/"><a>Inicio</a></Link>
+            <Link href="/matery"><a>Materias</a></Link>
+            <Link href="/horary"><a>Horarios</a></Link>
+            <Link href="/didactic"><a>Material didactico</a></Link>
+        </nav>
+    );
+    
+}
+
+
+export default Navbar;
