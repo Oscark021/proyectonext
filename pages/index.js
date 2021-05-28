04c9072704f507d49c8077dae5a02eb2acfb3e74
Julio Olvera <39699586+HoNoJulzz@users.noreply.github.com>
2021-05-27 22:05:55 -0500

    :lipstick: Vista de las flexcards

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,12 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-import Navbar from '../Components/Navbar'
+import FlexCard from '../Components/Flex-card'
 
 export default function Home() {
+
+  const cards = [{ src: '/img/historia.jpg' }, { src: '/img/historia.jpg' }, { src: '/img/historia.jpg' }]
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,6 +16,13 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
+
+
+        <div className="carousel-img">
+          { cards.map((card, index) => (
+            <FlexCard imgSrc={ card.src } />
+          )) }
+        </div>
         
       </main>
 
