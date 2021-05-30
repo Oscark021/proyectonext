6bf74a23b8e8d3c0777453913ffb3ed4736e544d
Oscark021 <oscarvelasco169@gmail.com>
2021-05-30 17:08:04 -0500

    :triangular_flag_on_post: Actualizar etiquetas

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -3,10 +3,11 @@ import Link from 'next/link'
 const Navbar = () => {
     return(
         <nav>
-            <Link href="/"><a>Inicio</a></Link>
-            <Link href="/matery"><a>Materias</a></Link>
-            <Link href="/horary"><a>Horarios</a></Link>
-            <Link href="/didactic"><a>Material didactico</a></Link>
+            <Link href="/"><a>Inicio |</a></Link>
+            <Link href="/matery"><a> Materias |</a></Link>
+            <Link href="/horary"><a> Horarios |</a></Link>
+            <Link href="/didactic"><a>Material didactico |</a></Link>
+            <Link href="/didactic"><a>Iniciar sesion</a></Link>
         </nav>
     );
     
